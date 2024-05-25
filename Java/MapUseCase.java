66ef0dfa8e63ac717b619b562fca099c2f497c48
Kalyan.Kanuri <kalyankanuri497@gmail.com>
2024-05-25 14:45:23 +0530

    Learn Map Collection in Java

---
+++ Java/MapUseCase.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class ExecMap {
+    public void execMap() {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("India", 130);
+        map.put("China", 140);
+        map.put("USA", 100);
+        map.put("UK", 90);
+
+        System.out.println(map);
+    }
+}
+
+
+public class MapUseCase {
+    public static void main(String[] args) {
+        ExecMap em = new ExecMap();
+        em.execMap();
+    }
+}
